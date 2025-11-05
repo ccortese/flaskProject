2fc9b82d30c27fd065e3d514074daa8915ff9c59
ccortese <chasec1g@gmail.com>
2025-11-04 23:03:07 -0500

    Add simple test file
    This file contains minor code style issues to test the automated review system:
    - Functions using camelCase instead of snake_case naming
    - Missing docstrings for functions
    
    This should trigger the automated review agent to provide feedback.

---
+++ simple_test.py
@@ -0,0 +1,12 @@
+"""
+Simple test file for the automated PR review system
+"""
+
+def calculateSum(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    return total
+
+def getUserName(firstName, lastName):
+    return firstName + " " + lastName
